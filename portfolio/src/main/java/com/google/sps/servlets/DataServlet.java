13bec98f90145b538104a7514199ab9a49b0483a
Aminu Abdusalam <aminuabdusalam2019@gmail.com>
2022-06-21 03:00:58 +0000

    Modified DataServlet and script.js

--- portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
@@ -19,14 +19,24 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.util.ArrayList;
+import com.google.gson.Gson;;
+
 
 /** Servlet that returns some example content. TODO: modify this file to handle comments data */
 @WebServlet("/data")
 public class DataServlet extends HttpServlet {
 
+  ArrayList<String> messages = new ArrayList<String>();
+     
   @Override
   public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    messages.add("This is Aminu");
+    messages.add("the one and only");
+    messages.add("any other one is counterfeit");
+    Gson gson = new Gson();
+    String json = gson.toJson(messages);
     response.setContentType("text/html;");
-    response.getWriter().println("<h1>Hello world!</h1>");
+    response.getWriter().println(json);
   }
 }
